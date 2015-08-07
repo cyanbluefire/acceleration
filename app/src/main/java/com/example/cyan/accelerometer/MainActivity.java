f71d595d27e243e04f227d427e463525bd0b0023
崔焱 <cyan@cuiyandeMac-mini-2.local>
2015-08-07 18:06:05 +0800

    add

--- app/src/main/java/com/example/cyan/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/cyan/accelerometer/MainActivity.java
@@ -10,6 +10,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends Activity {
@@ -17,36 +24,99 @@ public class MainActivity extends Activity {
     public SensorManager mSensorManager;
     public Sensor mSensor;
     public static final  String TAG = "MainActivity";
+    public static final double calibration = SensorManager.STANDARD_GRAVITY;
+    private TextView tv_currentACC;
+    private TextView tv_maxACC;
+    private TextView tv_x;
+    private TextView tv_y;
+    private TextView tv_z;
+    private float acceleration_x;
+    private float acceleration_y;
+    private float acceleration_z;
+
+    public int left_down = 0;      //标识：向左下侧
+    public int left_up = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv_currentACC = (TextView)findViewById(R.id.currentAcceleration);
+        tv_maxACC = (TextView)findViewById(R.id.maxAcceleration);
+        tv_x = (TextView)findViewById(R.id.accelerationX);
+        tv_y = (TextView)findViewById(R.id.accelerationY);
+        tv_z = (TextView)findViewById(R.id.accelerationZ);
+
         mSensorManager= (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         mSensor =  mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-//        SensorEventListener listener;
-        mSensorManager.registerListener(listener,mSensor,SensorManager.SENSOR_DELAY_NORMAL);
+//      SensorEventListener listener;
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                updateGUI();
+            }
+        },0,100);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(listener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
+
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(listener);
+        super.onPause();
+    }
+
     protected SensorEventListener listener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
             int sensorType = sensorEvent.sensor.getType();
             float[] values = sensorEvent.values;
 
-            float x = values[0];
-            float y = values[1];
-            float z = values[2];
+            acceleration_x = values[0];
+            acceleration_y = values[1];
+            acceleration_z = values[2];
 
-            Log.i(TAG,"x::"+x+" y::"+y+"z::"+z);
-            Log.i(TAG,"Math.abs(x)"+Math.abs(x)+"Math.abs(y):" +Math.abs(y) + "Math.abs(z):" + Math.abs(z));
-            if(sensorType == Sensor.TYPE_ACCELEROMETER){
-                int value = 15; //阀值
-                if(x >=value || x <= -value || y >= value || y <= -value || z >= value || z <= -value){
-                    Log.e(TAG,"检测到摇动");
-                }
+            Log.i(TAG,"x::"+acceleration_x+" y::"+acceleration_y+"z::"+acceleration_z);
+//            Log.i(TAG,"Math.abs(x)"+Math.abs(x)+"Math.abs(y):" +Math.abs(y) + "Math.abs(z):" + Math.abs(z));
+//            if(sensorType == Sensor.TYPE_ACCELEROMETER){
+//                int value = 15; //阀值
+//                if(acceleration_x >=value || acceleration_x <= -value || acceleration_y >= value || acceleration_y <= -value || acceleration_z >= value || acceleration_z <= -value){
+//                    Log.e(TAG,"检测到摇动");
+//                }
+//            }
+//            left_down = 0;      //标识：向左下侧
+//            left_up = 0;
+            if(acceleration_x >5 && acceleration_z < 9) {
+                left_down = 1;
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        left_down = 0;
+                    }
+                }, 500);
+                Log.e(TAG,"左 下 侧");
+//                Toast.makeText(MainActivity.this,"左 下 侧",Toast.LENGTH_SHORT).show();
             }
+            if(acceleration_x <-5 && acceleration_z < 8) {
+                left_up = 1;
+                new Timer().schedule(new TimerTask(){
+                    @Override
+                    public void run() {
+                        left_up = 0;
+                    }
+                },500);
+                Log.e(TAG,"左 上 侧");
+//                Toast.makeText(MainActivity.this,"左 上 侧",Toast.LENGTH_SHORT).show();
+            }
+
 
         }
 
@@ -56,6 +126,20 @@ public class MainActivity extends Activity {
         }
     };
 
+    private void updateGUI(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tv_x.setText(String.valueOf(acceleration_x));
+                tv_x.invalidate();
+                tv_y.setText(String.valueOf(acceleration_y));
+                tv_y.invalidate();
+                tv_z.setText(String.valueOf(acceleration_z));
+                tv_z.invalidate();
+
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
